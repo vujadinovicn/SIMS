8fd17054d49190376858a3e40d4a0ac56d612f68
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-06-25 16:01:04 +0200

    Added repositories for devices and toll cards

--- TollStations/TollStations/Core/Devices/Repository/DeviceRepository.cs
+++ TollStations/TollStations/Core/Devices/Repository/DeviceRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -9,12 +10,13 @@ using TollStations.Core.Devices;
 
 namespace TollStations.Core.Devices.Repository
 {
-    public class DeviceRepository
+    public class DeviceRepository : IDeviceRepository
     {
+        private String _fileName = @"..\..\..\Data\devices.json";
+
         private int _maxId;
-        private String _fileName = @"..\..\Data\devices.json";
         public List<Device> Devices { get; set; }
-        public Dictionary<int, Device> DevicesById { get; set; }
+        public Dictionary<int, Device> DeviceById { get; set; }
 
         private JsonSerializerOptions _options = new JsonSerializerOptions
         {
@@ -22,6 +24,78 @@ namespace TollStations.Core.Devices.Repository
             PropertyNameCaseInsensitive = true
         };
 
+        public DeviceRepository()
+        {
+            this.Devices = new List<Device>();
+            this.DeviceById = new Dictionary<int, Device>();
+            this._maxId = 0;
+            this.LoadFromFile();
+        }
 
+        public void LoadFromFile()
+        {
+            var devices = JsonSerializer.Deserialize<List<Device>>(File.ReadAllText(_fileName), _options);
+            foreach (Device device in devices)
+            {
+                if (device.Id > _maxId)
+                {
+                    _maxId = device.Id;
+                }
+                this.Devices.Add(device);
+                this.DeviceById.Add(device.Id, device);
+            }
+        }
+
+        public void Save()
+        {
+            var allLocations = JsonSerializer.Serialize(this.Devices, _options);
+            File.WriteAllText(this._fileName, allLocations);
+        }
+
+        public List<Device> GetAll()
+        {
+            return this.Devices;
+        }
+
+        public Dictionary<int, Device> GetAllById()
+        {
+            return this.DeviceById;
+        }
+
+        public Device GetById(int id)
+        {
+            if (DeviceById.ContainsKey(id))
+                return DeviceById[id];
+            return null;
+        }
+
+        public Device Add(Device device)
+        {
+            this._maxId++;
+            int id = this._maxId;
+            device.Id = id;
+
+            this.Devices.Add(device);
+            this.DeviceById.Add(device.Id, device);
+            Save();
+            return device;
+        }
+
+        public void Update(int id, Device byDevice)
+        {
+            Device device = GetById(id);
+            device.Id = byDevice.Id;
+            device.Type = byDevice.Type;
+            device.IsValid = byDevice.IsValid;
+            Save();
+        }
+
+        public void Delete(int id)
+        {
+            Device device = GetById(id);
+            this.Devices.Remove(device);
+            this.DeviceById.Remove(id);
+            Save();
+        }
     }
 }
---
+++ TollStations/TollStations/Core/TollCards/Repository/ITollCardRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using TollStations.Core.TollCards.Model;
+
+namespace TollStations.Core.TollCards.Repository
+{
+    interface ITollCardRepository
+    {
+        List<TollCard> TollCards { get; set; }
+        Dictionary<int, TollCard> TollCardsById { get; set; }
+
+        TollCard Add(TollCard card);
+        List<TollCard> GetAll();
+        Dictionary<int, TollCard> GetAllById();
+        TollCard GetById(int id);
+        void LoadFromFile();
+        void Save();
+    }
+}
