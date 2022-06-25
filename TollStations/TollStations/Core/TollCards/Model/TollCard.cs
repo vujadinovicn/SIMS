15163f621f6e18b2959bcefe9e8d40265964c08b
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-06-25 14:08:42 +0200

    Added tollCard and tollPayment classes

---
+++ TollStations/TollStations/Core/TollCards/Model/TollCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollStations.Core.TollCards.Model
+{
+    class TollCard
+    {
+        DateTime time;
+        string plate;
+
+        public TollCard(DateTime time, string plate)
+        {
+            this.time = time;
+            this.plate = plate;
+        }
+
+        public DateTime Time { get => time; set => time = value; }
+        public string Plate { get => plate; set => plate = value; }
+    }
+}
---
+++ TollStations/TollStations/Core/TollPayments/Model/TollPayment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TollStations.Core.SystemUsers.Cashiers.Model;
+using TollStations.Core.TollCards.Model;
+
+namespace TollStations.Core.TollPayments.Model
+{
+    public enum Currency
+    {
+        RSD,
+        EUR
+    }
+
+    class TollPayment
+    {
+        DateTime time;
+        Currency currency;
+        double amount;
+        Cashier cashier;
+        TollCard tollCard;
+
+        public TollPayment(DateTime time, Currency currency, double amount, Cashier cashier, TollCard tollCard)
+        {
+            this.time = time;
+            this.currency = currency;
+            this.amount = amount;
+            this.cashier = cashier;
+            this.tollCard = tollCard;
+        }
+
+        public DateTime Time { get => time; set => time = value; }
+        public Currency Currency { get => currency; set => currency = value; }
+        public double Amount { get => amount; set => amount = value; }
+        public Cashier Cashier { get => cashier; set => cashier = value; }
+        internal TollCard TollCard { get => tollCard; set => tollCard = value; }
+    }
+}
